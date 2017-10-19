d57e3c64b42e9d8f42e07057e9047f5256d1c473
Steffen Brand <s.brand@vcare24.de>
2017-10-19 17:37:46 +0200

    bumped version, updated dependencies

--- test/LatestResultIntegrationTest.php
+++ test/LatestResultIntegrationTest.php
@@ -28,6 +28,16 @@ class LatestResultIntegrationTest extends TestCase
     public function testGetLatestResultByKey()
     {
         $result = $this->client->getLatestResultByKey('MY-PLAN');
+
+        $result->getNumber();
+        $result->getState();
+        $result->getLink()->getHref();
+        $result->getPlan()->getKey();
+        $result->getPlan()->getName();
+        $result->getPlan()->getShortKey();
+        $result->getPlan()->getShortName();
+        $result->getPlan()->getLink()->getHref();
+
         $this->assertNotNull($result);
     }
 }
